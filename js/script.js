ad04463db0e9f804ea7314b0fe0384360ce9b125
John Minchala <johnpminweb@gmail.com>
2020-04-19 03:07:27 -0400

    added css

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@ var car = {
   make: "BMW",
   type: "Series 5",
   color: "Gray",
-  isTurnedOn: true,
+  isTurnedOn: false,
   numberOfWheels: 4,
   numberOfSeats: ["seat 1",
                   "seat 2",
                   "seat 3",
-                  "seat 4" ]
+                  "seat 4" ],
+  carOn: function(){
+    this.isTurnedOn = true;
+  }
+  
 };
