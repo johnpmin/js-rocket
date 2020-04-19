d28ff560dd0837b1f96b966ff09970e088ebc065
John Minchala <johnpminweb@gmail.com>
2020-04-19 02:28:18 -0400

    added js to html

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var car = {
+  make: "BMW",
+  type: "Series 5",
+  color: "Gray",
+  isTurnedOn: true,
+  numberOfWheels: 4,
+  numberOfSeats: ["seat 1",
+                  "seat 2",
+                  "seat 3",
+                  "seat 4" ]
+};
